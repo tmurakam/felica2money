fa58cc8e450a587c3fd0b25d2afc67cb74cedfd0
Takuya Murakami <tmurakam@tmurakam.org>
2008-03-15 12:04:59 +0000

    updated
    git-svn-id: http://svn.sourceforge.jp/svnroot/moneyimport/trunk/FeliCa2Money.net@119 2eb6a366-1e22-0410-b8ed-a9c1f8270cfe

--- CsvRules.cs
+++ CsvRules.cs
@@ -18,7 +18,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-// CSV 変換ルール
+// CSV 変換ルールセット
 
 using System;
 using System.Collections.Generic;
@@ -32,6 +32,11 @@ namespace FeliCa2Money
     {
         private List<CsvRule> ruleList;
 
+        public CsvRules()
+        {
+            ruleList = new List<CsvRule>();
+        }
+
         // 定義ファイルを読み込む
         public void LoadFromFile(string path)
         {
@@ -41,7 +46,6 @@ namespace FeliCa2Money
 
             XmlNodeList list = root.GetElementsByTagName("Rule");
 
-            ruleList = new List<CsvRule>();
             for (int i = 0; i < list.Count; i++)
             {
                 CsvRule rule = new CsvRule();
@@ -52,11 +56,11 @@ namespace FeliCa2Money
 
                     switch (e.Name)
                     {
+                        case "Ident":
+                            rule.Org = value;
+                            break;
                         case "Name":
                             rule.Name = value;
-                            break;
-                        case "Org":
-                            rule.Org = value;
                             break;
                         case "BankId":
                             rule.BankId = int.Parse(value);
@@ -123,200 +127,4 @@ namespace FeliCa2Money
             return null;
         }
     }
-        
-    public class CsvRule
-    {
-        private string org; // 組織名
-        private int bankId; // 銀行ID
-        private string name;  // 銀行名
-        private string firstLine; // １行目
-        private bool isAscent;    // 昇順かどうか
-
-        private System.Collections.Hashtable colHash = new System.Collections.Hashtable(); // カラムのマッピング
-
-        private DateTime prevDate;
-        private int idSerial;
-
-        public string Org
-        {
-            get { return org; }
-            set { org = value; }
-        }
-        public int BankId {
-            get { return bankId; }
-            set { bankId = value; }
-        }
-        public string Name {
-            get { return name; }
-            set { name = value; }
-        }
-        public string FirstLine
-        {
-            get { return firstLine; }
-            set { firstLine = value; }
-        }
-        public string Order
-        {
-            set
-            {
-                if (value == "Descent") isAscent = false;
-                else isAscent = true;
-            }
-        }
-        public bool IsAscent
-        {
-            get { return isAscent; }
-        }
-        public string Format
-        {
-            set
-            {
-                SetFormat(value);
-            }
-        }
-        
-        // フォーマット解析
-        public void SetFormat(string format)
-        {
-            string[] cols = format.Split(new Char[] { ',' });
-
-            for (int i = 0; i < cols.Length; i++)
-            {
-                colHash[cols[i]] = i;
-            }
-        }
-
-        // リセット
-        public void Reset()
-        {
-            prevDate = new DateTime(1900, 1, 1, 0, 0, 0);
-            idSerial = 0;
-        }
-
-        // 指定したカラムを取得
-        private string getCol(string[] row, string key)
-        {
-            if (colHash[key] == null)
-            {
-                return null;
-            }
-            int col = (int)colHash[key];
-            return row[col];
-        }
-
-        private int getColInt(string[] row, string key)
-        {
-            string v = getCol(row, key);
-            if (v == null) return 0;
-
-            //return int.Parse(v, System.Globalization.NumberStyles.AllowThousands);
-            return int.Parse(v);
-        }
-
-        // データ解析
-        public Transaction parse(string[] row)
-        {
-            Transaction t = new Transaction();
-
-            // TODO
-            // quote を削除する
-
-            // 日付
-            string date = getCol(row, "Date");
-            if (date != null)
-            {
-                t.date = parseDate(date);
-            }
-            else {
-                int year = getColInt(row, "Year");
-                int month = getColInt(row, "Month");
-                int day = getColInt(row, "Day");
-
-                if (year == 0 || month == 0 || day == 0) {
-                    return null;
-                }
-
-                if (year < 100)
-                {
-                    year += 2000;
-                }
-                t.date = new DateTime(year, month, day, 0, 0, 0);
-            }
-
-            // ID
-            string id = getCol(row, "Id");
-            if (id != null)
-            {
-                t.id = getColInt(row, "Id");
-            }
-            else
-            {
-                // 日付毎に ID を振る
-                if (t.date == prevDate)
-                {
-                    idSerial++;
-                }
-                else
-                {
-                    prevDate = t.date;
-                    idSerial = 0;
-                }
-                t.id = idSerial;
-            }
-
-            // 金額
-            t.value = getColInt(row, "Income");
-            t.value -= getColInt(row, "Outgo");
-
-            // 残高
-            t.balance = getColInt(row, "Balance");
-            
-            // 適用
-            t.desc = getCol(row, "Desc");
-
-            // 備考
-            t.memo = getCol(row, "Memo");
-
-            // トランザクションタイプを自動設定
-            t.GuessTransType(t.value >= 0);
-            
-            return t;
-        }
-
-        private DateTime parseDate(string date)
-        {
-            int year, month, day;
-
-            // '/' で区切られている場合
-            string[] split = date.Split(new Char[] { '/', '.', ' ' });
-            if (split.Length >= 3)
-            {
-                year = int.Parse(split[0]);
-                month = int.Parse(split[1]);
-                day = int.Parse(split[2]);
-            }
-            else
-            {
-                date = split[0];
-
-                if (date.Length != 6 && date.Length != 8)
-                {
-                    // パース不可能
-                    // TBD
-                    throw new Exception("不明なフォーマット");
-                }
-
-                int n = int.Parse(date);
-                year = n / 10000;
-                month = (n / 100) % 100;
-                day = n % 100;
-            }
-
-            if (year < 100) {
-                year += 2000;
-            }
-            return new DateTime(year, month, day, 0, 0, 0);
-        }
-    }
-
 }
