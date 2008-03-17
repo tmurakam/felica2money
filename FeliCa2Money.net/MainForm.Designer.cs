216357bf2fcb7075916a3ba3f29b3a849d18f423
Takuya Murakami <tmurakam@tmurakam.org>
2008-03-17 17:12:13 +0000

    misc fix
    git-svn-id: http://svn.sourceforge.jp/svnroot/moneyimport/trunk@154 2eb6a366-1e22-0410-b8ed-a9c1f8270cfe

--- FeliCa2Money.net/MainForm.Designer.cs
+++ FeliCa2Money.net/MainForm.Designer.cs
@@ -35,18 +35,14 @@ namespace FeliCa2Money
             this.buttonOption = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.buttonManual = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.buttonNanaco = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.buttonCSV = new System.Windows.Forms.Button();
             this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
@@ -92,40 +88,20 @@ namespace FeliCa2Money
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(76, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(226, 12);
+            this.label1.Size = new System.Drawing.Size(266, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "PaSoRiを使って Edy の利用履歴を Microsoft";
+            this.label1.Text = "PaSoRiを使って Edy の利用履歴を Microsoft Money に取り込みます。";
             this.label1.UseWaitCursor = true;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(76, 87);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(271, 12);
+            this.label2.Size = new System.Drawing.Size(266, 29);
             this.label2.TabIndex = 3;
-            this.label2.Text = "PaSoRiを使って Suica/ICOCAなど交通系電子マネーの";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(76, 99);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(234, 12);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "利用履歴を Microsoft Money に取り込みます。";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(76, 38);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(121, 12);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Money に取り込みます。";
+            this.label2.Text = "PaSoRiを使って Suica/ICOCAなど交通系電子マネーの利用履歴を Microsoft Money に取り込みます。";
             // 
             // label5
             // 
@@ -167,21 +143,11 @@ namespace FeliCa2Money
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(76, 154);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(243, 12);
+            this.label7.Size = new System.Drawing.Size(266, 29);
             this.label7.TabIndex = 12;
-            this.label7.Text = "PaSoRiを使って nanaco の利用履歴を Microsoft";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(76, 166);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(121, 12);
-            this.label8.TabIndex = 13;
-            this.label8.Text = "Money に取り込みます。";
+            this.label7.Text = "PaSoRiを使って nanaco の利用履歴を Microsoft Money に取り込みます。";
             // 
             // saveFileDialog
             // 
@@ -199,21 +165,11 @@ namespace FeliCa2Money
             // 
             // label9
             // 
-            this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(76, 219);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(215, 12);
+            this.label9.Size = new System.Drawing.Size(266, 28);
             this.label9.TabIndex = 15;
-            this.label9.Text = "CSVファイルを読み込んで Microsoft Money";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(76, 231);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(84, 12);
-            this.label10.TabIndex = 16;
-            this.label10.Text = "に取り込みます。";
+            this.label9.Text = "CSVファイルを読み込んで Microsoft Money に取り込みます。";
             // 
             // openFileDialog
             // 
@@ -226,17 +182,13 @@ namespace FeliCa2Money
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(366, 439);
-            this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.buttonCSV);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.buttonNanaco);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.buttonManual);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonOption);
@@ -262,18 +214,14 @@ namespace FeliCa2Money
         private System.Windows.Forms.Button buttonOption;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button buttonManual;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button buttonNanaco;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
         private System.Windows.Forms.Button buttonCSV;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
