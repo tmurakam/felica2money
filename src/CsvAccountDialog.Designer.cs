e4299ad98c1b5aaf8f41ad03123773b30cac42e3
Takuya Murakami <tmurakam@tmurakam.org>
2011-02-13 15:26:26 +0900

    add CsvAccountDialog

---
+++ src/CsvAccountDialog.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace FeliCa2Money
+{
+    partial class CsvAccountDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAddAccount = new System.Windows.Forms.Button();
+            this.buttonModifyAccount = new System.Windows.Forms.Button();
+            this.buttonDeleteAccount = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Location = new System.Drawing.Point(93, 34);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(252, 232);
+            this.listBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "口座一覧";
+            // 
+            // buttonAddAccount
+            // 
+            this.buttonAddAccount.Location = new System.Drawing.Point(12, 34);
+            this.buttonAddAccount.Name = "buttonAddAccount";
+            this.buttonAddAccount.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddAccount.TabIndex = 2;
+            this.buttonAddAccount.Text = "口座追加";
+            this.buttonAddAccount.UseVisualStyleBackColor = true;
+            // 
+            // buttonModifyAccount
+            // 
+            this.buttonModifyAccount.Location = new System.Drawing.Point(12, 63);
+            this.buttonModifyAccount.Name = "buttonModifyAccount";
+            this.buttonModifyAccount.Size = new System.Drawing.Size(75, 23);
+            this.buttonModifyAccount.TabIndex = 3;
+            this.buttonModifyAccount.Text = "口座修正";
+            this.buttonModifyAccount.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteAccount
+            // 
+            this.buttonDeleteAccount.Location = new System.Drawing.Point(12, 92);
+            this.buttonDeleteAccount.Name = "buttonDeleteAccount";
+            this.buttonDeleteAccount.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeleteAccount.TabIndex = 4;
+            this.buttonDeleteAccount.Text = "口座削除";
+            this.buttonDeleteAccount.UseVisualStyleBackColor = true;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Location = new System.Drawing.Point(93, 295);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 5;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(194, 295);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 6;
+            this.buttonCancel.Text = "キャンセル";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // CsvAccountDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 337);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonDeleteAccount);
+            this.Controls.Add(this.buttonModifyAccount);
+            this.Controls.Add(this.buttonAddAccount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox);
+            this.Name = "CsvAccountDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSV資産選択";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAddAccount;
+        private System.Windows.Forms.Button buttonModifyAccount;
+        private System.Windows.Forms.Button buttonDeleteAccount;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ src/CsvAccountDialog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FeliCa2Money
+{
+    public partial class CsvAccountDialog : Form
+    {
+        private CsvAccountManager mManager;
+
+        public CsvAccountDialog(CsvAccountManager manager)
+        {
+            InitializeComponent();
+
+            mManager = manager;
+
+            listBox.Items.Clear();
+            string[] names = mManager.getNames();
+            foreach (string name in names)
+            {
+                listBox.Items.Add(name);
+            }
+        }
+
+        // 指定されたアカウントを選択状態にする
+        public void SelectAccount(CsvAccount account)
+        {
+            if (account == null) return;
+
+            int idx = mManager.IndexOf(account);
+            listBox.SelectedIndex = idx;
+        }
+        
+        // 選択中のアカウントを返す
+        public CsvAccount SelectedAccount()
+        {
+            int idx = listBox.SelectedIndex;
+            if (idx < 0)
+            {
+                return null;
+            }
+            return mManager.GetAt(idx);
+        }
+            
+    }
+}
