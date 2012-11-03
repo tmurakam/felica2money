3d85d78f334f83832639dfa24acefc345e5215ca
Takuya Murakami <tmurakam@tmurakam.org>
2012-11-03 22:51:39 +0900

    add transaction tests

---
+++ src/FeliCa2Money.test/TransactionTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace FeliCa2Money.test
+{
+    [TestFixture]
+    class TransactionTest
+    {
+        Transaction t;
+
+        [SetUp]
+        public void SetUp()
+        {
+            t = new Transaction();
+            t.date = new DateTime(2000, 1, 1);
+        }
+
+        // MD5計算。文字列の MD5 を計算して 16進数で返す。
+        private string md5(string s)
+        {
+            System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(s));
+
+            StringBuilder result = new StringBuilder();
+            foreach (byte b in hash)
+            {
+                result.Append(b.ToString("x2"));
+            }
+            return result.ToString();
+        }
+
+        [Test]
+        public void testTransIdWithoutId()
+        {
+            t.date = new DateTime(2012, 12, 31);
+            t.serial = 567;
+            t.value = -1234;
+            t.desc = "説明";
+            t.memo = "メモ";
+
+            string tid = t.transId();
+            Assert.IsNotNull(tid);
+            string expected = md5("2012;12;31;567;-1234;説明;メモ");
+            Assert.AreEqual(expected, tid);
+        }
+    }
+}
